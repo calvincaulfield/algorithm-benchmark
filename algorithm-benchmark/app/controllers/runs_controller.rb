476190fd5dd477784a8198aba872ad4756041e6b
Ubuntu <ubuntu@ip-172-31-20-248.ap-northeast-2.compute.internal>
2018-03-21 06:40:04 +0000

    Implementing post API

--- algorithm-benchmark/app/controllers/runs_controller.rb
+++ algorithm-benchmark/app/controllers/runs_controller.rb
@@ -1,54 +1,83 @@
 class RunsController < ApplicationController
-	def index
-		@runs = Run.all
-	end
-	
-	def new
-		@run = Run.new
-	end
-	
-	def edit
-		@run = Run.find(params[:id])
-	end
-	
-	def show
-		@run = Run.find(params[:id])
-	end
-	
-	def create
-        #render plain: run_params
-        raw_params = run_params
-        raw_params[:tags] = JSON.parse(raw_params[:tags])
-        raw_params[:data] = JSON.parse(raw_params[:data])
-		@run = Run.new(raw_params)
-		
-		if @run.save
-			redirect_to runs_path
-		else
-			render 'new'
-		end
-	end
-	
-	def update
-		@run = Run.find(params[:id])
-		
-		if @run.update(run_params)
-			redirect_to runs_path
-		else	
-			render 'edit'
-		end
-	end
-	
-	def destroy
-		@run = Run.find(params[:id])
-		@run.destroy
-		
-		redirect_to runs_path
-	end
-	
-	private
-		def run_params
-			params.require(:run).permit(
-				:title, :date, :env, :tags, :data)
-		end
+  skip_before_action :verify_authenticity_token
+  before_action :use_unsafe_params, only: [:api]
+
+  def index
+    @runs = Run.all
+  end
+
+  def new
+    @run = Run.new
+  end
+
+  def edit
+    @run = Run.find(params[:id])
+  end
+
+  def show
+    @run = Run.find(params[:id])
+  end
+
+  def create
+    #render plain: run_params
+    raw_params = run_params
+    raw_params[:tags] = JSON.parse(raw_params[:tags])
+    raw_params[:data] = JSON.parse(raw_params[:data])
+    @run = Run.new(raw_params)
+
+    if @run.save
+      redirect_to runs_path
+    else
+      render 'new'
+    end
+  end
+
+  def api
+    #JSON.parse(params)
+    #render plain: params[:run]
+
+    raw_params = params[:run]
+    #raw_params[:tags] = JSON.parse(raw_params[:tags])
+    #raw_params[:data] = JSON.parse(raw_params[:data])
+    @run = Run.new(raw_params)
+    @run.save
+    #if @run.save
+    #	redirect_to runs_path
+    #else
+    #	render 'new'
+    #end
+  end
+
+  def update
+    @run = Run.find(params[:id])
+
+    if @run.update(run_params)
+      redirect_to runs_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @run = Run.find(params[:id])
+    @run.destroy
+
+    redirect_to runs_path
+  end
+
+  def params
+    @_dangerous_params || super
+  end
+
+  private
+
+  def run_params
+    params.require(:run).permit(
+      :title, :date, :env, {:tags => []}, :comment, :data
+    )
+  end
+
+  def use_unsafe_params
+    @_dangerous_params = request.parameters
+  end
 end
--- algorithm-benchmark/db/schema.rb
+++ algorithm-benchmark/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318090252) do
+ActiveRecord::Schema.define(version: 20180321004253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20180318090252) do
     t.jsonb "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "comment"
+  end
+
+  create_table "test", id: false, force: :cascade do |t|
+    t.jsonb "j"
   end
 
 end
