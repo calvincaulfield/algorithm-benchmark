523b683dd471cee51dafed0f40cd1ec04f86a8b3
Ubuntu <ubuntu@ip-172-31-20-248.ap-northeast-2.compute.internal>
2018-03-23 03:05:51 +0000

    changed urls of Apache websites to new domain. Added dev- prefix to urls to enable admin mode in Rails apps

--- algorithm-benchmark/app/controllers/application_controller.rb
+++ algorithm-benchmark/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :admin_url
+
+  def admin_url
+    "dev-bench.minami.im"
+  end
+
 end
