af290a14b397be627a5097315f36bbf41168b3e7
Ubuntu <ubuntu@ip-172-31-20-248.ap-northeast-2.compute.internal>
2018-04-10 08:37:36 +0000

    Allowed new category route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   get '/categories', to: 'categories#index'
+  get '/categories/new', to: 'categories#new'
+  post '/categories', to: 'categories#create'
+
   get '/categories/:id', to: 'categories#show', as: 'category'
   get '/categories/:id/edit', to: 'categories#edit', as: 'edit_category'
 
